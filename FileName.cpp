6d2b2b6d4cca021521354b55965f990db7afdc1d
bongpodoal <76566179+bongpodoal@users.noreply.github.com>
2024-01-02 11:34:32 +0900

    기말고사 6번

--- FileName.cpp
+++ FileName.cpp
@@ -1,5 +1,14 @@
 #include <stdio.h>
 int main()
 {
-	printf("Hi");
+	int a = 0;
+	int b = 1;
+	int c = 0;
+	for (int i = 2;i <= 10;i++)
+	{
+		c = a + b;
+		printf("%d %d %d\n", a, b, c);
+		a = b;
+		b = c;
+	}
 }
