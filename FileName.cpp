a53c1313aa93c746e4573f762719c064eb3d92ac
bongpodoal <76566179+bongpodoal@users.noreply.github.com>
2024-01-03 09:55:58 +0900

    2024-01-02 숙제2

--- FileName.cpp
+++ FileName.cpp
@@ -1,21 +1,28 @@
 #include <stdio.h>
 int main()
 {
-	int test[10][10] = { 0, };
-	for (int i = 0; i < 10;i++)
-	{
-		for (int j = 0; j < 10;j++)
-		{
-			if (i == j || 9-i == j)
-				test[i][j] = i+1;
-		}
-	}
-	for (int i = 0; i < 10;i++)
-	{
-		for (int j = 0; j < 10;j++)
-		{
-			printf("%d", test[i][j]);
-		}
-		printf("\n");
-	}
+    for (int i = 1;i <= 10;i += 2)
+    {
+        for (int j = 1;j < 10 - i;j += 2)
+        {
+            printf(" ");
+        }
+        for (int j = 1;j <= i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i = 1;i <= 10 ;i += 2)
+    {
+        for (int j = 1;j <= i;j += 2)
+        {
+            printf(" ");
+        }
+        for (int j = 1;j <= (10 - i) - 2;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
 }
