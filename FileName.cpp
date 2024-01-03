c36ad90052a5dbd4ad78facb07c5b07f95af02e7
bongpodoal <76566179+bongpodoal@users.noreply.github.com>
2024-01-03 13:32:21 +0900

    2024-01-03 계산기 과제

--- FileName.cpp
+++ FileName.cpp
@@ -1,22 +1,105 @@
 #include <stdio.h>
-int get_integer(void)
+#include <math.h>
+int menu(void)
 {
 	int n;
-	printf("정수를 입력하세요: ");
+	printf("\n==================\n");
+	printf("1. 덧셈\n");
+	printf("2. 뺄셈\n");
+	printf("3. 곱셈\n");
+	printf("4. 나눗셈\n");
+	printf("5. 제곱근\n");
+	printf("6. 팩토리얼\n");
+	printf("7. 사인함수\n");
+	printf("다른숫자: 나가기\n");
+	printf("==================\n");
+	printf("숫자를 입력하세요: ");
 	scanf_s("%d", &n);
 	return n;
 }
-void is_prime(int n)
+void add(void)
 {
-	for (int i = 2;i < n;i++) 
-		if (n % i == 0) {
-			printf("%d는 소수가 아닙니다", n);
-			return;
-		}
-	printf("%d는 소수입니다", n);
-	return;
+	int x, y;
+	printf("정수 2개를 입력하세요: ");
+	scanf_s("%d %d",&x, &y);
+	printf("결과 = %d", x + y);
+}
+void sub(void)
+{
+	int x, y;
+	printf("정수 2개를 입력하세요: ");
+	scanf_s("%d %d", &x, &y);
+	printf("결과 = %d", x - y);
+}
+void mul(void)
+{
+	int x, y;
+	printf("정수 2개를 입력하세요: ");
+	scanf_s("%d %d", &x, &y);
+	printf("결과 = %d", x * y);
+}
+void div(void)
+{
+	int x, y;
+	printf("정수 2개를 입력하세요: ");
+	scanf_s("%d %d", &x, &y);
+	printf("결과 = %d", x / y);
+}
+
+void sqr(void)
+{
+	double x;
+	printf("실수 1개를 입력하세요: ");
+	scanf_s("%lf", &x);
+	printf("결과 = %lf", sqrt(x));
+}
+void fac(void)
+{
+	int n;
+	long long fac = 1;
+	printf("정수 1개를 입력하세요: ");
+	scanf_s("%d", &n);
+	for (int i = 1; i <= n;i++)
+	{
+		fac *= i;
+	}
+	printf("결과 = %lld", fac);
+}
+void sn(void)
+{
+	double n;
+	printf("실수 1개를 입력하세요: ");
+	scanf_s("%lf", &n);
+	printf("결과 = %lf", sin(n));
 }
 int main() {
-	int n = get_integer();
-	is_prime(n);
+	while (1) {
+		switch (menu()) {
+		case 1:
+			add();
+			break;
+		case 2:
+			sub();
+			break;
+		case 3:
+			mul();
+			break;
+		case 4:
+			div();
+			break;
+		case 5:
+			sqr();
+			break;
+		case 6:
+			fac();
+			break;
+		case 7:
+			sn();
+			break;
+		default:
+			return 0;
+			break;
+		}
+
+	}
 }
