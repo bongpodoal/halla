a22fe2ccb69ff5bcfb6e70dfcaf4d86d5025774e
bongpodoal <76566179+bongpodoal@users.noreply.github.com>
2024-01-03 10:25:26 +0900

    2024-01-02 숙제2 배열

--- FileName.cpp
+++ FileName.cpp
@@ -1,28 +1,39 @@
 #include <stdio.h>
-int main()
-{
-    for (int i = 1;i <= 10;i += 2)
-    {
-        for (int j = 1;j < 10 - i;j += 2)
-        {
-            printf(" ");
+
+int main() {
+    char str[10][10];
+
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            str[i][j] = ' ';
         }
-        for (int j = 1;j <= i;j++)
-        {
-            printf("*");
+    }
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 5 - i - 1; j > 0; j--) {
+            str[i][j] = ' ';
+        }
+        for (int j = 5 - i - 1; j <= 5 + i - 1; j++) {
+            str[i][j] = '*';
+        }
+    }
+
+    for (int i = 0; i < 5 - 1; i++) {
+        for (int j = 0; j <= i; j++) {
+            str[5 + i][j] = ' ';
+        }
+        for (int j = i + 1; j < 2 * 5 - i - 2; j++) {
+            str[5 + i][j] = '*';
+        }
+    }
+
+    for (int i = 0; i < 2 * 5 - 1; i++) {
+        for (int j = 0; j < 2 * 5; j++) {
+            printf("%c", str[i][j]);
         }
         printf("\n");
     }
-    for (int i = 1;i <= 10 ;i += 2)
-    {
-        for (int j = 1;j <= i;j += 2)
-        {
-            printf(" ");
-        }
-        for (int j = 1;j <= (10 - i) - 2;j++)
-        {
-            printf("*");
-        }
-        printf("\n");
-    }
+
+    return 0;
 }
