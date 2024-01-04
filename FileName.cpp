0749a0829119faf1529afaf701b6c557691cadd3
bongpodoal <76566179+bongpodoal@users.noreply.github.com>
2024-01-04 11:34:21 +0900

    2024-01-04 과제2

--- FileName.cpp
+++ FileName.cpp
@@ -4,21 +4,28 @@
 int main(void)
 {
 	srand(time(NULL));
-	int scores[20][5];
-	double total[20];
-
-	for (int i = 0; i < 20; i++)
+	int even[10] = { 0, };
+	int odd[10] = { 0, };
+	int random = 0;
+	int even_num = 0;
+	int odd_num = 0;
+	while (odd_num != 10 && even_num != 10)
 	{
-		for (int j = 0; j < 4;j++)
+		random = rand()%1000;
+		if (random % 2 == 0)
 		{
-			scores[i][j] = rand() % 100 + 1;
+			even[even_num] = random;
+			even_num++;
 		}
-		scores[i][4] = rand() % 10 + 1;
+		else
+		{
+			odd[odd_num] = random;
+			odd_num++;
+		}
 	}
-	for (int i = 0;i < 20;i++)
-	{
-		total[i] = scores[i][0] * 0.3 + scores[i][1] * 0.4 + scores[i][2] * 0.2 + scores[i][3] * 0.1 - scores[i][4];
-		printf("학생 #%2d의 최종성석 = %.2lf\n", i + 1, total[i]);
-	}
+	if (odd_num == 10)
+		printf("홀수가 먼저 채워졌습니다. 짝수는 %d개 입니다.", even_num);
+	else
+		printf("짝수가 먼저 채워졌습니다. 홀수는 %d개 입니다.", odd_num);
 
 }
