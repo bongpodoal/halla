b7f528d61b307db89a319b14608974ce766faf48
bongpodoal <76566179+bongpodoal@users.noreply.github.com>
2024-01-04 12:38:08 +0900

    2024-01-04 과제3

--- FileName.cpp
+++ FileName.cpp
@@ -1,31 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int main(void)
-{
-	srand(time(NULL));
-	int even[10] = { 0, };
-	int odd[10] = { 0, };
-	int random = 0;
-	int even_num = 0;
-	int odd_num = 0;
-	while (odd_num != 10 && even_num != 10)
-	{
-		random = rand()%1000;
-		if (random % 2 == 0)
-		{
-			even[even_num] = random;
-			even_num++;
-		}
-		else
-		{
-			odd[odd_num] = random;
-			odd_num++;
-		}
-	}
-	if (odd_num == 10)
-		printf("홀수가 먼저 채워졌습니다. 짝수는 %d개 입니다.", even_num);
-	else
-		printf("짝수가 먼저 채워졌습니다. 홀수는 %d개 입니다.", odd_num);
 
+int main(void) {
+    srand(time(NULL));
+    int num[10];
+    int random, j;
+
+    num[0] = rand() % 100 + 1;
+    for (int i = 1; i < 10; i++) {
+        random = rand() % 100 + 1; // 랜덤값 생성
+
+
+        int index = 0;
+        for (int k = 0; k < i; k++) {        // 만약 난수가 값보다 크면, 그만큼 인덱스를 증가
+            if (num[k] <= random) {
+                index = k + 1;
+            }
+        }
+
+        // 찾은 위치로부터 뒤에 있는 모든 요소들을 한 칸씩 뒤로 이동
+        for (int j = i; j > index; j--) {
+            num[j] = num[j - 1];
+        }
+
+        // 난수를 삽입
+        num[index] = random;
+    }
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", num[i]); // 값 출력
+    }
+    return 0;
 }
