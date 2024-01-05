2adb79853d1437f6dd30e61636dfe8173ce1efe3
bongpodoal <76566179+bongpodoal@users.noreply.github.com>
2024-01-05 14:04:35 +0900

    2024-01-05 학생의 성적 처리 프로그램

--- FileName.cpp
+++ FileName.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
-int search_binary(int list[], int n, int key) {
+#include <stdlib.h>
+#include <time.h>
+int search_binary(double list[], int n, double key) {
 	int low = 0;
 	int high = n - 1;
 	int middle;
 	while (low <= high) {
-		printf("[%d %d]\n", low, high);
 		middle = (low + high) / 2;
 		if (key == list[middle])
 			return middle;
@@ -14,17 +14,65 @@ int search_binary(int list[], int n, int key) {
 		else
 			low = middle + 1;
 	}
-	return -1;
 }
-int main(void) {
-	int list[9] = { 1, 3, 5, 6, 7, 9, 11, 20, 30 };
-	int n;
-	for (int i = 0; i < 9; i++)
+int main()
+{
+	srand(time(NULL));
+	double scores[20][7];
+	double sort_scores[20];
+	int rank[20];
+	int temp;
+	int low = 0;
+	int high = 19;
+	int middle;
+	double key;
+
+
+	for (int i = 0; i < 20; i++)
 	{
-		printf("%d ", list[i]);
+		for (int j = 0; j < 4;j++)
+		{
+			scores[i][j] = rand() % 100 + 1;
+		}
 	}
-	printf("\n\n위 배열에서 찾으실 정수를 입력하세요: ");
-	scanf_s("%d", &n);
-	int num = search_binary(list, 9, n);
-	printf("인덱스 %d번째에 있습니다.", num);
+
+	printf("\n");
+	for (int i = 0;i < 20;i++) // 총점계산
+	{
+		scores[i][5] = scores[i][0] * 0.3 + scores[i][1] * 0.4 + scores[i][2] * 0.2 + scores[i][3] * 0.1;
+	} 
+
+
+	for (int i = 0; i < 20;i++) // 등수 계산을 위한 배열 복사
+	{
+		sort_scores[i] = scores[i][5];
+
+	} 
+
+	for (int i = 0;i < 19;i++)  // 총점 정렬
+	{
+		for (int j = i + 1;j <= 19;j++)
+		{
+			if (sort_scores[i] > sort_scores[j])
+			{
+				temp = sort_scores[i];
+				sort_scores[i] = sort_scores[j];
+				sort_scores[j] = temp;
+
+			}
+		}
+	}
+
+	for (int i = 0; i < 20;i++)
+	{
+		key = scores[i][5];
+		scores[i][6] = search_binary(sort_scores, 20, key);
+	}
+
+	for (int i = 0;i < 20;i++)
+	{
+		printf("%4d번 학생: 중간: %4.0lf, 기말: %4.0lf, 과제: %4.0lf,  출석: %4.0lf, 총점: %4.3lf, %4.0lf등   \n", i+1,scores[i][0], scores[i][1],scores[i][2], scores[i][3], scores[i][5], scores[i][6]+1);
+	}
+
+
 }
