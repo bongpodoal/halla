a066f7e46ffdaca2611478e9d9534561e79fed3f
bongpodoal <76566179+bongpodoal@users.noreply.github.com>
2024-01-09 13:45:21 +0900

    2024-01-09 C로 stack구현 2

--- study.cpp
+++ study.cpp
@@ -17,16 +17,59 @@ void push(int stack[], int value)
 void pop(int stack[])
 {
 	if (is_empty(stack)) return;
-	else stack[--top];
+	else stack[top--];
+}
+int menu(void)
+{
+	int n;
+	printf("\n================\n");
+	printf("1. push\n");
+	printf("2. pop\n");
+	printf("3. check\n");
+	printf("4. End\n");
+	printf("================\n");
+	printf("작업을 입력하세요: ");
+	scanf_s("%d", &n);
+	return n;
 }
 int main()
 {
 	int stack[MAX_STACK_SIZE];
+	int n;
+	while (1) {
+		switch (menu()) {
 
-	push(stack, 1);
-	printf("%d\n", stack[top]);
-	push(stack, 2);
-	printf("%d\n", stack[top]);
-	pop(stack);
-	printf("%d\n", stack[top]);
+		case 1:
+			printf("값을 입력하세요: ");
+			scanf_s("%d", &n);
+			push(stack, n);
+			break;
+		case 2:
+			if (is_empty(stack)) {
+				printf("스택이 비어있어 pop 할 수 없습니다.");
+				break;
+			}
+			else {
+				printf("스택의 가장 위의 값 %d를 pop 했습니다.", stack[top]);
+				pop(stack);
+				break;
+			}
+		case 3:
+			if (is_empty(stack) == 0) {
+				printf("현재 스택의 가장 위의 값은 %d입니다.\n", stack[top]);
+				break;
+			}
+			else
+			{
+				printf("스택이 비어있습니다.");
+				break;
+			}
+		case 4:
+			printf("종료합니다.\n");
+			return 0;
+		}
+
+
+	}
+	
 }
