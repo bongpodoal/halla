d9304a2fd9944e8197ca87efeeb76f5223aba05c
bongpodoal <76566179+bongpodoal@users.noreply.github.com>
2025-09-08 10:18:05 +0900

    선형 테이블 요소 삽입 (동적 할당)

--- study.cpp
+++ study.cpp
@@ -1,28 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX_SIZE 20
-void InsertInArray(int* array, int length, int num, int idx)
+int* InsertInArray(int* array, int length, int num, int idx)
 {
 	int count;
-
-	for (count = length; count > idx; count--)
+	int* tempArray = (int*)malloc(sizeof(int) * (length + 1));
+	for (count = 0; count < length; count++)
 	{
-		array[count] = array[count - 1];
+		if (count < idx)
+			tempArray[count] = array[count];
+		else
+			tempArray[count + 1] = array[count];
 	}
-	array[idx] = num;
+	tempArray[idx] = num;
+	free(array);
 
+	return tempArray;
 }
 int main(void)
 {
-	int scores[MAX_SIZE] = { 90,70,50,80,60,85 };
+	int tempArray[] = { 90,70,50,80,60,85 };
+	int* scores = (int*)malloc(sizeof(int) * 6);
 	int length = 6;
 	int count;
 
-	InsertInArray(scores, length, 75, 2);
+	for (count = 0; count < length; count++)
+		scores[count] = tempArray[count];
+
+	scores = InsertInArray(scores, length, 75, 2);
 	length += 1;
 
 	for (count = 0; count < length; count++)
 		printf("%d, ", scores[count]);
+
+	free(scores);
+	
 
 	return 0;
 
