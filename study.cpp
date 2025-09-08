714ae7af1e11ac0188f0e2b53abed474ad3b6ec9
bongpodoal <76566179+bongpodoal@users.noreply.github.com>
2025-09-08 09:49:39 +0900

    선형 테이블 요소 삽입

--- study.cpp
+++ study.cpp
@@ -1,36 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int* AppendToArray(int* array, int length, int num)
+#define MAX_SIZE 20
+void InsertInArray(int* array, int length, int num, int idx)
 {
 	int count;
-	int* tempArray = (int*)malloc(sizeof(int) * (length + 1));
 
-	for (count = 0, count < length; count++)
-		tempArray[count] = array[count];
+	for (count = length; count > idx; count--)
+	{
+		array[count] = array[count - 1];
+	}
+	array[idx] = num;
 
-	tempArray[length] = num;
-
-	free(array);
-	return tempArray;
 }
 int main(void)
 {
-	int count;
-	int array[] = { 90,70,50,80,60,85 };
-	int* scores = (int*)malloc(sizeof(int) * 6);
+	int scores[MAX_SIZE] = { 90,70,50,80,60,85 };
 	int length = 6;
+	int count;
+
+	InsertInArray(scores, length, 75, 2);
+	length += 1;
 
 	for (count = 0; count < length; count++)
-		scores[count] = array[count];
-
-	scores = AppendToArray(scores, length++, 75); 
-	// 증감 연산자가 뒤에 있어 6이 전달되고 1이 더해짐
-
-	for(count = 0; count < length; count++)
 		printf("%d, ", scores[count]);
-
-	free(scores);
 
 	return 0;
 
