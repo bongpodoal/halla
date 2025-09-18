d07a0816c25ae746db84083e711e0e6ac5905e19
bongpodoal <76566179+bongpodoal@users.noreply.github.com>
2025-09-18 15:58:01 +0900

    Fisher-Yates Shuttle 알고리즘

--- study.cpp
+++ study.cpp
@@ -1,21 +1,29 @@
 #include <stdio.h>
-int Absolute(int value)
-{
-	if (value > 0)
-		return value;
-	else
-		return value * -1;
+#include <stdlib.h>
+#include <time.h>
+
+void rand_arr(int arr[], int N) {
+    for (int i = 0; i < N; i++) {
+        arr[i] = i;
+    }
+
+    for (int i = N - 1; i > 0; i--) {
+        int j = rand() % N;
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
 }
-int main()
-{
-	int x, y;
-	scanf_s("%d %d", &x, &y);
-	for (int i = 0; i < x;i++)
-	{
-		for (int j = 0; j < y;j++)
-		{
-			printf("%d ", Absolute(i - j) + 1);
-		}
-		printf("\n");
-	}
+
+int main() {
+    int arr[9];
+    int N = sizeof(arr) / sizeof(int);
+
+    srand(time(NULL)); 
+    rand_arr(arr, N);
+
+    for (int i = 0; i < N; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
 }
