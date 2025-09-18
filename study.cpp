d83c7751ee8933a95b54657c1274820abb0a98be
bongpodoal <76566179+bongpodoal@users.noreply.github.com>
2025-09-18 15:27:48 +0900

    2차원배열활용1

--- study.cpp
+++ study.cpp
@@ -1,27 +1,20 @@
-
 #include <stdio.h>
+int Absolute(int value)
+{
+	if (value > 0)
+		return value;
+	else
+		return value * -1;
+}
 int main()
 {
-	int length = 6;
-	int map[5][5] = { 0, };
-	for (int i = 0; i < length - 1; i++)
+	int x, y;
+	scanf_s("%d %d", &x, &y);
+	for (int i = 0; i < x;i++)
 	{
-		for (int j = 0; j < length - 1; j++)
+		for (int j = 0; j < y;j++)
 		{
-			int far = i - j;
-			if (far < 0)
-				far *= -1;
-			map[i][j] = far + 1;
-
-		}
-	}
-	
-
-	for (int i = 0; i < length - 1; i++)
-	{
-		for (int j = 0; j < length - 1; j++)
-		{
-			printf("%d", map[i][j]);
+			printf("%d ", Absolute(i - j) + 1);
 		}
 		printf("\n");
 	}
