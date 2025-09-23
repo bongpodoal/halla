5648b0a1a3f0de1e9a88f1bf6da80631717637a1
bongpodoal <76566179+bongpodoal@users.noreply.github.com>
2025-09-23 19:17:20 +0900

    알고리즘 2차과제 최종

--- study.cpp
+++ study.cpp
@@ -1,29 +1,62 @@
 #include <stdio.h>
-void bubble(int* arrays, int length)
+
+#define MAX_SIZE 50
+
+void Print_Square(int square[MAX_SIZE][MAX_SIZE],int n )
 {
-	for (int i = 0; i < length - 1; i++)
-	{
-		for (int j = 0; j < length - i - 1; j++)
-		{
-			if (arrays[j] > arrays[j + 1])
-			{
-				int temp = arrays[j];
-				arrays[j] = arrays[j + 1];
-				arrays[j + 1] = temp;
-			}
-		}
-	}
+    int i, j;
+    for (i = 0; i < n; i++) 
+    {
+        for (j = 0; j < n; j++) 
+        {
+            printf("%4d", square[i][j]);
+        }
+        printf("\n");
+    }
 }
-int main()
+void Create_Square(int square[MAX_SIZE][MAX_SIZE], int n)
 {
-	int scores[] = { 60,50,95,80,70 };
-	int length = sizeof(scores) / sizeof(int);
+    int i, j;
+    for (i = 0; i < n; i++) 
+    {
+        for (j = 0; j < n; j++) 
+        {
+            square[i][j] = 0;
+        }
+    }
 
-	bubble(scores, length);
+    // 시작 위치
+    int row = n - 1;
+    int col = n / 2;
+    int num;
 
-	for (int i = 0; i < length; i++)
-	{
-		printf("%d ", scores[i]);
-	}
-	return 0;
+    for (num = 1; num <= n * n; num++) 
+    {
+        square[row][col] = num;
+
+        int nrow = (row + 1) % n;
+        int ncol = (col - 1 + n) % n;  // 왼쪽 아래 방향으로 이동
+
+        // 다음 위치가에 이미 값이 있다면, 
+        if (square[nrow][ncol] != 0) 
+            row = (row - 1 + n) % n; // 현재 위치에서 바로 위로 이동
+        else 
+        {
+            row = nrow;
+            col = ncol;
+        }
+    }
+}
+
+int main() 
+{
+    int n;
+    int square[MAX_SIZE][MAX_SIZE]; // 정적 2차원 배열 선언
+
+    printf("마방진의 크기를 입력하세요 : ");
+    scanf_s("%d", &n);
+    Create_Square(square, n);
+    Print_Square(square,n);
+
+    return 0;
 }
