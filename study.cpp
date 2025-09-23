c941642132111734840e01596b22de1ed0554c34
bongpodoal <76566179+bongpodoal@users.noreply.github.com>
2025-09-23 20:55:53 +0900

    과제1

--- study.cpp
+++ study.cpp
@@ -1,62 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
-#define MAX_SIZE 50
-
-void Print_Square(int square[MAX_SIZE][MAX_SIZE],int n )
-{
-    int i, j;
-    for (i = 0; i < n; i++) 
+#include <string.h>
+int main(void) {
+    int map[3][3] = { 0, };
+    int big_map[5][5] = { 0, };
+    for (int i = 0; i < 3; ++i) 
     {
-        for (j = 0; j < n; j++) 
+        for (int j = 0; j < 3; ++j) 
         {
-            printf("%4d", square[i][j]);
+            int row = i + j;           
+            int col = 2 + (i - j);      
+            int v = i * 3 + j + 1;     
+            big_map[row][col] = v;
         }
+    }
+
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            int v = big_map[i][j];
+            if (!v) 
+                continue;
+            int row = (i + 2) % 3;
+            int col = (j + 2) % 3;
+            map[row][col] = v; 
+        }
+    }
+
+
+    printf("5x5 큰 배열:\n");
+    for (int r = 0; r < 5; ++r) 
+    {
+        for (int c = 0; c < 5; ++c) printf("%2d ", big_map[r][c]);
+            printf("\n");
+    }
+
+    printf("\n3x3 마방진:\n");
+    for (int r = 0; r < 3; ++r) 
+    {
+        for (int c = 0; c < 3; ++c) printf("%2d ", map[r][c]);
         printf("\n");
     }
-}
-void Create_Square(int square[MAX_SIZE][MAX_SIZE], int n)
-{
-    int i, j;
-    for (i = 0; i < n; i++) 
-    {
-        for (j = 0; j < n; j++) 
-        {
-            square[i][j] = 0;
-        }
-    }
-
-    // 시작 위치
-    int row = n - 1;
-    int col = n / 2;
-    int num;
-
-    for (num = 1; num <= n * n; num++) 
-    {
-        square[row][col] = num;
-
-        int nrow = (row + 1) % n;
-        int ncol = (col - 1 + n) % n;  // 왼쪽 아래 방향으로 이동
-
-        // 다음 위치가에 이미 값이 있다면, 
-        if (square[nrow][ncol] != 0) 
-            row = (row - 1 + n) % n; // 현재 위치에서 바로 위로 이동
-        else 
-        {
-            row = nrow;
-            col = ncol;
-        }
-    }
-}
-
-int main() 
-{
-    int n;
-    int square[MAX_SIZE][MAX_SIZE]; // 정적 2차원 배열 선언
-
-    printf("마방진의 크기를 입력하세요 : ");
-    scanf_s("%d", &n);
-    Create_Square(square, n);
-    Print_Square(square,n);
 
     return 0;
 }
